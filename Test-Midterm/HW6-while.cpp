5c6e06a95334ff9d841c366f91eeee5c11d5cf4a
PattaraponR <162939578+PattaraponR@users.noreply.github.com>
2025-09-03 23:59:53 +0700

    Update HW6-while.cpp

--- Test-Midterm/HW6-while.cpp
+++ Test-Midterm/HW6-while.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
-    int n;
-    printf("Enter number :\n");
+int main() {
+    int n, i, j;
+
+    printf("Enter number:\n");
+
     if (scanf("%d", &n) != 1) {
-        printf("Please Enter number\n");
+        printf("โปรดใส่เลขจำนวนเต็ม\n");
         return 0;
     }
 
-    int i = n;
+    i = n;
     while (i >= 2) {
-        int j = 2;
+        j = 2;
         while (j * j <= i && i % j != 0) {
             j++;
         }
-        if (j * j > i) {        
-            printf("%d ", i);   
+        if (j * j > i) {
+            printf("%d ", i);
         }
         i--;
     }
-    printf("\n");
+
     return 0;
 }
