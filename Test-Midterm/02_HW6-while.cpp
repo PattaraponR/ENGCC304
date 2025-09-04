c2f089d07899bdd1817467fba2f21c695319fff3
PattaraponR <162939578+PattaraponR@users.noreply.github.com>
2025-09-04 09:55:22 +0700

    Update 02_HW6-while.cpp

--- Test-Midterm/02_HW6-while.cpp
+++ Test-Midterm/02_HW6-while.cpp
@@ -2,23 +2,29 @@
 
 int main() {
     int n, i, j;
+    int checkPrime;
 
     printf("Enter number:\n");
 
     if (scanf("%d", &n) != 1) {
         printf("โปรดใส่เลขจำนวนเต็ม\n");
-    }
-
-    i = n;
-    while (i >= 2) {
-        j = 2;
-        while (j * j <= i && i % j != 0) {
-            j++;
+    } else {
+        i = n;
+        while (i >= 2) {
+            checkPrime = 1;
+            j = 2;
+            while (j * j <= i) {
+                if (i % j == 0) {
+                    checkPrime = 0;
+                    break;
+                }
+                j++;
+            }
+            if (checkPrime) {
+                printf("%d ", i);
+            }
+            i--;
         }
-        if (j * j > i) {
-            printf("%d ", i);
-        }
-        i--;
     }
 
     return 0;
