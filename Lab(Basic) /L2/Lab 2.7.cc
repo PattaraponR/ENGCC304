5c08ad5f64e13f47b4a852e2dde8af5da6761aa7
PattaraponR <162939578+PattaraponR@users.noreply.github.com>
2025-12-30 02:27:41 +0700

    Update Lab 2.7.cc

--- Lab(Basic) /L2/Lab 2.7.cc
+++ Lab(Basic) /L2/Lab 2.7.cc
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    int N, i;
+    int FB = 0;
+    int F = 0;
+    int B = 0;
+    int O = 0;
+
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+
+    for (i = 1; i <= N; i++) {
+        if (i % 15 == 0) {
+            FB++;
+        } else if (i % 3 == 0) {
+            F++;
+        } else if (i % 5 == 0) {
+            B++;
+        } else {
+            O++;
+        }
+    }
+
+    printf("Count FizzBuzz (by 15): %d\n", FB);
+    printf("Count Fizz (by 3 only): %d\n", F);
+    printf("Count Buzz (by 5 only): %d\n", B);
+    printf("Count Other: %d\n", O);
+    return 0;
+}
