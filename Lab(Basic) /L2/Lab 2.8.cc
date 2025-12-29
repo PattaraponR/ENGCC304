4610cb374ceac8f14725f12a62f0a1df1b7780a6
PattaraponR <162939578+PattaraponR@users.noreply.github.com>
2025-12-30 02:32:35 +0700

    Update Lab 2.8.cc

--- Lab(Basic) /L2/Lab 2.8.cc
+++ Lab(Basic) /L2/Lab 2.8.cc
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+int main() {
+    int N, i;
+    int sID, score, tC, aC;
+    float attP;
+    int pC = 0;
+    int fC = 0;
+
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+
+    for (i = 0; i < N; i++) {
+        if (scanf("%d %d %d %d", &sID, &score, &tC, &aC) != 4) {
+            break;
+        }
+
+        attP = (float)(tC - aC) / tC * 100.0;
+
+        if (score >= 50 && attP >= 75.0) {
+            printf("Student %d: PASS\n", sID);
+            pC++;
+        } else {
+            printf("Student %d: FAIL - ", sID);
+            fC++;
+
+            int lowS = (score < 50);
+            int lowA = (attP < 75.0);
+
+            if (lowS && lowA) {
+                printf("Both Score and Attendance\n");
+            } else if (lowS) {
+                printf("Low Score\n");
+            } else {
+                printf("Low Attendance (%.2f%%)\n", attP);
+            }
+        }
+    }
+
+    printf("--- Summary ---\n");
+    printf("Total PASS: %d\n", pC);
+    printf("Total FAIL: %d\n", fC);
+    return 0;
+}
